df863cc040b93db9c404b51b5bf6e7121935a866
pchmn <couleurwhatever@148.60.14.93>
2015-12-02 17:10:23 +0100

    ConcurrentArrayList

--- CSR_TP/src/festival/internals/SiteDepart.java
+++ CSR_TP/src/festival/internals/SiteDepart.java
@@ -1,14 +1,16 @@
 package festival.internals;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class SiteDepart {
 
 	public List<Bus> buses;
 	
 	public SiteDepart() {
-		buses = new ArrayList<Bus>();
+		buses = new CopyOnWriteArrayList<Bus>();
 	}
 	
 	/**
@@ -17,13 +19,13 @@ public class SiteDepart {
 	 */
 	public synchronized void monterBus(Festivalier f){
 		
-		// Tant qu'aucun bus ne lui est attribué
+		// Tant qu'aucun bus ne lui est attribuï¿½
 		while(true) {
 			// TODO Concurrent ModificationException
-			// Parcours les différents bus
+			// Parcours les diffï¿½rents bus
 			for (Bus unBus : buses) {
 				
-				// Vérifie leurs disponibilités
+				// Vï¿½rifie leurs disponibilitï¿½s
 				if (!unBus.isOnTheRoadAgain() && unBus.getPlacesDispo() <= unBus.getPlacesMaxi() && unBus.getPlacesDispo() > 0){
 					
 					// Essaie d'attribuer un bus au festivalier
